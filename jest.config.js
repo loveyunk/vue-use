aeed8dad3cf20662a7ec68c9fa80563d08e04251
loveyunk <mjjccc@126.com>
2020-05-05 12:02:51 +0800

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 module.exports = {
+  preset: 'ts-jest',
   roots: ['<rootDir>/src'],
-  testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)',
-  ],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  // setupFiles: ['<rootDir>/packages/_docs/test.setup.js'],
+  coverageDirectory: 'coverage',
 };
